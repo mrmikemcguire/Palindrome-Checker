4b50aa998e9a836009be75c166798fed999518f1
mrmikemcguire <mcguire@mullenhigh.com>
2014-03-20 08:30:30 -0600

    Initial commit

---
+++ Palindrome Tester/src/PalindromeTester.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class PalindromeTester
+	{
+	static String originalText;
+	
+	public static void main(String[] args)
+		{
+		System.out.println("Please enter your word(s) to be tested: ");
+		Scanner userInput = new Scanner(System.in);
+		String originalText = userInput.nextLine();
+		userInput.close();
+		originalText = originalText.toLowerCase();
+		originalText = originalText.replace(" ","");
+		System.out.println("Your text without spaces is " + originalText);
+		
+		StringBuffer reversedText = new StringBuffer(originalText);
+		reversedText = reversedText.reverse();
+		System.out.println("Reversed, it reads " +reversedText);
+		System.out.println();
+		
+		if (originalText.contentEquals(reversedText))
+			{
+			System.out.println("It's a palindrome!");
+			}
+		else
+			{
+			System.out.println("Alas, it's not a palindome.");
+			}
+		}
+
+	}
