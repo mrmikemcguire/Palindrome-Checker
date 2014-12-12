c0cbfe37ae9172295d20765c0a4d4ec55111045e
mrmikemcguire <mcguire@mullenhigh.com>
2014-12-11 17:31:08 -0700

    Test

--- Palindrome Tester/src/PalindromeChecker.java
+++ Palindrome Tester/src/PalindromeChecker.java
@@ -21,12 +21,12 @@ public class PalindromeChecker
 	public static boolean checkForPalindrome(String originalText)
 		{
 		originalText = originalText.toLowerCase();
-		originalText = originalText.replace(" ","");
-		System.out.println("Your text without spaces is " + originalText);
+		originalText = originalText.replaceAll("[^\\p{L}\\p{Nd}]+", "");
+		System.out.println("Your stripped text is " + originalText);
 		
 		StringBuffer reversedText = new StringBuffer(originalText);
 		reversedText = reversedText.reverse();
-		System.out.println("Reversed, it reads " +reversedText);
+		System.out.println("Reversed, it reads " + reversedText);
 		System.out.println();
 		
 		if (originalText.contentEquals(reversedText))
