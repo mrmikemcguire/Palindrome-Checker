442911832d5603941bdc336b683d15cb62133c70
Mike McGuire <mcguire@mullenhigh.com>
2014-12-18 17:41:37 -0700

    Split four tests into four methods

--- Palindrome Tester/src/PalindromeCheckerTest.java
+++ Palindrome Tester/src/PalindromeCheckerTest.java
@@ -4,11 +4,26 @@ import org.junit.Test;
 public class PalindromeCheckerTest extends PalindromeChecker
 	{
 	@Test
-	public void PalindromCheckerTest()
+	public void TruePalindrome()
 		{
 		assertEquals(true, PalindromeChecker.checkForPalindrome("Abba"));
+		}
+	
+	@Test
+	public void FalsePalindrome()
+		{
 		assertEquals(false, PalindromeChecker.checkForPalindrome("Abbaa"));
+		}
+	
+	@Test
+	public void TestForSpaces()
+		{
 		assertEquals(true, PalindromeChecker.checkForPalindrome("race car"));
+		}
+	
+	@Test
+	public void TestForPunctuation()
+		{
 		assertEquals(true, PalindromeChecker.checkForPalindrome("A man, a plan, a canal - Panama"));
 		}
 
